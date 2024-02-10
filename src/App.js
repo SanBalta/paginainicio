edcc91bd3f223e378641c594a519604ea039eb67
Christian <gioosan30@hotmail.com>
2024-02-10 16:42:37 -0600

    Se agrego contenido a JS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,40 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Esta es mi aplicación</h1>
+        <html>
+      <body>
+
+      <div class="container1">
+        <h2>ESCUELA PREPARATORIA OFICIAL NO. 34</h2>
+        <img src="https://epo34info.files.wordpress.com/2013/09/escudo-aniv.jpg" alt="logo" />
+      </div>
+  
+    <main>
+      <div class="login">  
+        <form>
+          <div class="mensaje">
+            <p>Bienvenido</p>
+          </div>
+
+          <div class="email">
+            <label for="email">Email</label>
+            <input type ="email" id="email" name="email" required autofocus />
+          </div>
+              
+          <div class="pass">
+            <label for="pass">Contraseña</label>
+            <input type ="password" id="pass" name="pass" required />
+          </div>  
+
+          <div class="enviar">
+            <button type="submit">Login</button>
+          </div>
+        </form>  
+      </div>
+    </main>
+  </body>
+  </html>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
